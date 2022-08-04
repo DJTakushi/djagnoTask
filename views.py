1389a0b3e790590a4d172fc66c439bf38381cb77
DJTakushi <okinawanspud@gmail.com>
2022-08-04 11:44:50 -0500

    use class-based generics views

--- views.py
+++ views.py
@@ -133,25 +133,15 @@ def deletePost(request):
         pass
     return HttpResponseRedirect(reverse('djangoTask:index'))
 
-class todoList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class todoList(generics.ListCreateAPIView):
     """
     List all todos, or create a new todo.
     """
     queryset = todo.objects.all()
     serializer_class=todoSerializer
-    def get(self, request, *args, **kwargs):
-        return self.list(request,*args,**kwargs)
-    def post(self,request, *args,**kwargs):
-         return self.create(request,*args,**kwargs)
-class todoDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+class todoDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a code todo.
     """
     queryset = todo.objects.all()
     serializer_class=todoSerializer
-    def get(self,request,*args,**kwargs):
-        return self.retrieve(request,*args,**kwargs)
-    def put(self,request,*args,**kwargs):
-        return self.update(request,*args,**kwargs)
-    def delete(self,request,*args,**kwargs):
-        return self.destroy(request,*args,**kwargs)
